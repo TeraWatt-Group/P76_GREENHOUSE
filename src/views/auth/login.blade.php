9481636d35d652f6b727edbc76f01a74db15d3a9
andreyzaets <andrey.zayets@hotmail.com>
2021-09-15 11:54:42 +0300

    fix logo

--- src/views/auth/login.blade.php
+++ src/views/auth/login.blade.php
@@ -10,22 +10,22 @@
     <!-- Favicons and Apple Touch Icons -->
     <meta name="theme-color" content="#ffffff">
     <link rel="manifest" href="/manifest.json">
-    <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-touch-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-touch-icon-152x152.png">
-    <link rel="icon" type="image/png" sizes="196x196" href="/favicons/favicon-196x196.png">
-    <link rel="icon" type="image/png" sizes="160x160" href="/favicons/favicon-160x160.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
-    <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico">
+    <link rel="apple-touch-icon" sizes="57x57" href="/img/favicons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/img/favicons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/img/favicons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/img/favicons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/img/favicons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/img/favicons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/img/favicons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/img/favicons/apple-touch-icon-152x152.png">
+    <link rel="icon" type="image/png" sizes="196x196" href="/img/favicons/favicon-196x196.png">
+    <link rel="icon" type="image/png" sizes="160x160" href="/img/favicons/favicon-160x160.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/img/favicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/favicon-16x16.png">
+    <link rel="icon" type="image/x-icon" href="/img/favicons/favicon.ico">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="/favicons/mstile-144x144.png">
+    <meta name="msapplication-TileImage" content="/img/favicons/mstile-144x144.png">
 
     <title>{{ config('green.app_name') }}</title>
     <meta name="keywords" content="best business software, business software, software system">
@@ -35,7 +35,7 @@
     <meta property="og:description" content="{{ config('green.app_description') }}">
     <meta property="og:type" content="website">
     <meta property="og:url" content="{{ url()->full() }}">
-    <meta property="og:image" content="/favicons/logo.png" />
+    <meta property="og:image" content="/img/favicons/logo.png" />
     <meta property="og:site_name" content="{{ config('green.app_name') }}">
 
     <!-- Fonts -->
