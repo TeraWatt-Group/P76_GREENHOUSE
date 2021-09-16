c4694044c400c387deba24089277d020bdcd9922
andreyzaets <andrey.zayets@hotmail.com>
2021-09-16 16:32:21 +0300

    fix route

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,10 @@ Route::get('/', function(){
 	return view('welcome');
 })->name('welcome');
 
+Route::get('/home', function(){
+	return view('home');
+})->name('home');
+
 Route::group(['prefix' => 'green', 'as' => 'green.'], function () {
 
 });
